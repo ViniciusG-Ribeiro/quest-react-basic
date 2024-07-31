17dc3a2dd5d964990244d4fc821e74f68f3fef48
ViniciusG.Ribeiro <viniciusrib2014@hotmail.com>
2024-07-30 23:05:43 -0300

    limpando comentarios

--- src/components/input-text/input-text.js
+++ src/components/input-text/input-text.js
@@ -24,35 +24,3 @@ const InputText = () =>{
 }
 
 export default InputText;
-
-
-// import React, { useState } from "react";
-// import './input-text.css'
-
-// const InputText = (props) => {
-//     // Estado para armazenar o valor do input
-//     const [inputValue, setInputValue] = useState('');
-
-//     // Função para atualizar o estado com o valor do input
-//     const handleChange = (event) => {
-//         setInputValue(event.target.value);
-//     }
-
-//     return (
-//         <>
-//             <input
-//                 type="text"
-//                 className="inputtext"
-//                 placeholder="Write here"
-//                 value={inputValue} // Define o valor do input como o estado atual
-//                 onChange={handleChange}
-//             />
-
-//             <div className="container-text">
-//                 <h1>{inputValue}</h1> {/* Mostra o valor atual do input no h1 */}
-//             </div>
-//         </>
-//     );
-// }
-
-// export default InputText;
